84df547c632eb6e876af68e9644bab2df20fbbc2
leejunhee <725lsw@hanmail.net>
2018-10-23 14:07:34 +0900

    First commit

---
+++ app/src/main/java/com/swdevcamp/jun/swdevcamp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.swdevcamp.jun.swdevcamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
