17ab5d04c46e9e3eb4ce71eeba702be9d50d8a0e
leejunhee <725lsw@hanmail.net>
2018-10-23 16:07:00 +0900

    데이터 구조 생성

--- app/src/main/java/com/swdevcamp/jun/swdevcamp/MainActivity.java
+++ app/src/main/java/com/swdevcamp/jun/swdevcamp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.swdevcamp.jun.swdevcamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,LoadingActivity.class);
+        startActivity(intent);
+
+        RecyclerView recyclerView = findViewById(R.id.data_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+        ArrayList<DataCardItem> items = new ArrayList<>();
+        PersonalDataCardAdapter adapter = new PersonalDataCardAdapter(getApplicationContext(),items,R.layout.activity_main);
+        items.add(new DataCardItem(R.drawable.capture1,"capture1"));
+        items.add(new DataCardItem(R.drawable.capture1,"capture2"));
+        items.add(new DataCardItem(R.drawable.capture1,"capture3"));
+        items.add(new DataCardItem(R.drawable.capture1,"capture4"));
+
+        recyclerView.setAdapter(adapter);
     }
 }
